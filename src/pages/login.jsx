1053519dce1c25fc4eca5b2fab4dd416d22004e3
JuanX11 <negretenoguerajuanpablo@hotmail.com>
2024-02-01 19:33:33 -0500

    Commit Inicial

---
+++ src/pages/login.jsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <div class="bg-slate-800">
+      <h1 className="text-3xl font-bold underline">App</h1>
+      <h1 className="text-10xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
